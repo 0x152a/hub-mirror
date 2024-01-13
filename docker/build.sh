9841955049bbae1c176fe5abaf26239b4b3fb63e
0x152a <62497850+0x152a@users.noreply.github.com>
2024-01-13 18:54:53 +0800

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -2,6 +2,9 @@ REPO_NAME="suroi"
 TAG_NAME="1"
 
 git clone https://github.com/HasangerGames/suroi.git ./internal/app
+cd ./internal/app 
+git checkout v0.14.0
+cd ../..
 
 docker build -t "0x152a/$REPO_NAME:$TAG_NAME" ./internal
 docker push "0x152a/$REPO_NAME:$TAG_NAME"
