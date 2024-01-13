0d9f1218526b576bd3ad5b84a26029c4d2287e1d
0x152a <62497850+0x152a@users.noreply.github.com>
2024-01-13 22:58:09 +0800

    Update build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -1,9 +1,9 @@
-REPO_NAME="suroi"
-TAG_NAME="1"
+REPO_NAME="tldraw"
+TAG_NAME="v2.0.0-beta.2"
 
-git clone https://github.com/HasangerGames/suroi.git ./internal/app
+git clone https://github.com/tldraw/tldraw.git ./internal/app
 cd ./internal/app 
-git checkout v0.14.0
+git checkout v2.0.0-beta.2
 cd ../..
 
 docker build -t "0x152a/$REPO_NAME:$TAG_NAME" ./internal
